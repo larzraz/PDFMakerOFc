d09a8a242636fa455d086f23d6715d0191740095
larzraz <lars5030@edu.eal.dk>
2019-02-16 02:56:25 +0100

    added pdf

--- PDFMakerOFc/MainWindow.xaml.cs
+++ PDFMakerOFc/MainWindow.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using BitMiracle;
+using BitMiracle.Docotic.Pdf;
 
 namespace PDFMakerOFc
 {
@@ -50,10 +52,75 @@ namespace PDFMakerOFc
 
         private void GenerateList_Button_Click(object sender, RoutedEventArgs e)
         {
-            List<Player> selectedPlayers = playersDataGrid.SelectedItems.Cast<Player>().ToList();
+            List<Player> selectedPlayers = playersDataGrid.SelectedItems.Cast<Player>().OrderBy(x => x.Number).ToList();
             MatchNo = Int32.Parse(MatchNo_TextBox.Text);
             MatchDate = Match_calendar.DisplayDate;
             TeamNameString = teamsComboBox.Text;
+            string pathToFile = "C:/Users/Larz/Desktop/OFCEFBSS.pdf";
+
+            using (PdfDocument pdf = new PdfDocument("C:/Users/Larz/Desktop/OFCEFBS.pdf"))
+            {
+                foreach (PdfControl control in pdf.GetControls())
+                {
+                    switch (control.Name)
+                    {
+                        case "H-hold":
+                            if(TeamNameString == "Herre 2")
+                            {
+                                ((PdfTextBox)control).Text = "Odense Floorball Club 2";
+                                break;
+                            }
+                            else {
+                                ((PdfTextBox)control).Text = "Odense Floorball Club";
+                                break;
+                            }
+
+                        case "Række":
+                            if (TeamNameString == "Herre 2")
+                            {
+                                ((PdfTextBox)control).Text = "2. division Syd";
+                                break;
+                            }
+                            else
+                            {
+                                ((PdfTextBox)control).Text = "1. division Vest";
+                                break;
+                            }
+
+
+                        case "Spillested":
+                            ((PdfTextBox)control).Text = "Marienlystcentret";
+                            break;
+                        case "Kampnr":
+                            ((PdfTextBox)control).Text = " " + MatchNo.ToString();
+                            break;
+                        case "HN1":
+                            foreach (var player in selectedPlayers)
+                            {
+                                if (player.IsCaptain == true)
+                                {
+                                    ((PdfTextBox)control).Text = player.Number.ToString();
+                                    break;
+                                }
+                            }
+                            break;
+
+                        case "HS1":
+                            foreach (var player in selectedPlayers)
+                            {
+                                if (player.IsCaptain == true)
+                                {
+                                    ((PdfTextBox)control).Text = " " + player.Name;
+                                    break;
+                                }
+                            }
+                            break;
+
+                    }
+                }
+
+                pdf.Save(pathToFile);
+            }
         }
     }
 }
